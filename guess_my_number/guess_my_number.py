9d0346652138a6e3fe13915008fbf7fc49b4a51d
Cwelton1 <58041303+Cwelton1@users.noreply.github.com>
2024-05-01 17:18:33 +0100

    Add files via upload

---
+++ guess_my_number/guess_my_number.py
@@ -0,0 +1,20 @@
+import random # This is the only module you can import for this task
+
+# Write your function below for guess_my_number 
+print("please, think of a random number")
+low_range = int(input("Please input a low range, for example if your number is 50 type 1"))
+high_range = int(input("Please input a low range, for example if your number is 50 type 100"))
+
+Y_n = 'x'
+correct =  False
+num_of_guesses = 0;
+
+while correct == False:
+    guess = (high_range * low_range)
+
+
+
+
+# Program main -- Do not change code below
+guess_my_number()
+
